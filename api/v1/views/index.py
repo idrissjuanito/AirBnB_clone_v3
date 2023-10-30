ba019981d9a319d2b72422e6b70a641c89b06d81
idrissjuanito <idrissjuanito@gmail.com>
2023-10-30 17:12:42 +0000

    added stats route for sending count of each obj in storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,15 +5,25 @@ from models import storage
 from models.engine.db_storage import classes
 
 
+def pluralize(string):
+    """ gets the plural of a string """
+    if string[-1] == "y":
+        return string[:-1] + "ies"
+    else:
+        return string + "s"
+
+
 @app_views.route("/status")
 def api_status():
     """ reponds with a status of the api """
     return {"status": "OK"}
+
 
 @app_views.route("/stats")
 def obj_stats():
     """ counts the number of records for all objects """
     obj_counts = {}
     for cls in classes.values():
-        obj_counts[cls.__name__.lower()] = storage.count(cls)
+        obj_key = pluralize(cls.__name__.lower())
+        obj_counts[obj_key] = storage.count(cls)
     return obj_counts
